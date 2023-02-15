32ac3dd77ca9b2f7b0cf45c1d06512cfa833946e
Colten Van Voorhis <89817596+vanvocol001@users.noreply.github.com>
2023-02-14 20:05:31 -0600

    Project Prototype

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import uvicorn
+
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory='templates')
+
+app.mount(
+     '/static',
+     StaticFiles(directory=Path(__file__).parent.absolute()/'static'),
+     name = 'static'
+)
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse('index.html', context={'request': request})
+
+@app.get('/items/{item}')
+async def get_item(item : str):
+     return {'item name': item}
+
+if __name__ == '__main__':
+     uvicorn.run(app)
+
