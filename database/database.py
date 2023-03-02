6277db61d2e8df6f61c36bcdabc47228884a1491
Shawn Tucker <stucker1@leomail.tamuc.edu>
2023-03-02 13:44:34 -0600

    Hid credentials

--- database/database.py
+++ database/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import _credentials
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = "mysql://shawtrmg_group:passwordForCSCI440!@postgresserver/shawtrmg_inventory"
+SQLALCHEMY_DATABASE_URL = f"mysql://{_credentials.username}:{_credentials.password}@127.0.0.1/{_credentials.database}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
